c46fa5f6830958ddf8102c2f67be392452f6e8cf
irinakurnishova <kurnishovaiv@gmail.com>
2024-03-18 19:46:21 +0100

    routing and refactoring

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,11 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
+    Bar: typeof import('./src/components/Bar.vue')['default']
+    Bubble: typeof import('./src/components/Bubble.vue')['default']
+    Doughnut: typeof import('./src/components/Doughnut.vue')['default']
+    RouterLink: typeof import('vue-router')['RouterLink']
+    RouterView: typeof import('vue-router')['RouterView']
+    SideBar: typeof import('./src/components/SideBar.vue')['default']
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
 /**
  * main.js
  *
- * Bootstraps Vuetify and other plugins then mounts the App`
+ * Bootstraps Vuetify, Vue Router, and other plugins then mounts the App
  */
 
-// Plugins
 import { registerPlugins } from '@/plugins'
+import router from '@/router'
 
-// Components
 import App from './App.vue'
 
-// Composables
 import { createApp } from 'vue'
 
 const app = createApp(App)
 
 registerPlugins(app)
+
+app.use(router)
 
 app.mount('#app')
